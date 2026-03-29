04d2722499b894dc9e9137b3851d06583dfc9cd3
Calero <Jcaleriux@users.noreply.github.com>
2026-03-29 05:59:02 -0600

    Adición de carpeta data con algunos datos de referencia y cambio de path de users en ConfigurationItems.

--- src/ClassViews/Configuration/ConfigurationItems.cs
+++ src/ClassViews/Configuration/ConfigurationItems.cs
@@ -6,7 +6,7 @@
     public static class ConfigurationItems
     {
 
-        public static readonly string UserFilePath = @"C:\Users\Jose\OneDrive\Escritorio\Universidad Politécnico Internacional\1er cuatrimestre 2026\Técnicas de programación\Tecnicas1Q2026\TP-P1-Nutricion\users.csv";
+        public static readonly string UserFilePath = @"C:\Users\Jose\OneDrive\Escritorio\Universidad Politécnico Internacional\1er cuatrimestre 2026\Técnicas de programación\Tecnicas1Q2026\TP-P1-Nutricion\data\users.csv";
 
     }
 }
