7189a1d0989e5a77ac1738e201c3a74a5c5326a6
Calero <Jcaleriux@users.noreply.github.com>
2026-03-29 23:35:11 -0600

    Expansión de clase User

--- src/ClassModels/User.cs
+++ src/ClassModels/User.cs
@@ -1,6 +1,6 @@
 ﻿namespace ClassModels
 {
-        /// <summary>
+    /// <summary>
     /// Model to represetn the user of the system.
     /// </summary>
     public class User
@@ -8,12 +8,42 @@
         /// <summary>
         /// Initializes a new instance of the <see cref="User"/> class.
         /// </summary>
-        /// <param name="UserName">Name of the user.</param>
+        /// <param name="UserId">The user identifier.</param>
+        /// <param name="Name">The name.</param>
+        /// <param name="Email">The email.</param>
         /// <param name="Password">The password.</param>
-        public User( string UserName, string Password) 
+        /// <param name="Goal">The goal.</param>
+        /// <param name="ActivityLevel">The activity level.</param>
+        /// <param name="WeightKg">The weight kg.</param>
+        /// <param name="HeightCm">The height cm.</param>
+        /// <param name="Age">The age.</param>
+        /// <param name="Sex">The sex.</param>
+        /// <param name="DietType">Type of the diet.</param>
+        public User( 
+            int UserId, 
+            string Name,
+            string Email,
+            string Password,
+            string Goal,
+            string ActivityLevel,
+            decimal WeightKg,
+            decimal HeightCm,
+            int Age,
+            string Sex,
+            string DietType
+            ) 
         { 
-            this.UserName = UserName;
+            this.UserId = UserId;
+            this.Name = Name;
+            this.Email = Email;
             this.Password = Password;
+            this.Goal = Goal;
+            this.ActivityLevel = ActivityLevel;
+            this.WeightKg = WeightKg;
+            this.HeightCm = HeightCm;
+            this.Age = Age;
+            this.Sex = Sex;
+            this.DietType = DietType;
         }
 
         /// <summary>
@@ -22,17 +52,41 @@
         /// <param name="userData">The user data.</param>
         public User(string[] userData)
         {
-            this.UserName = userData[0];
-            this.Password = userData[1];
+            this.UserId = int.Parse(userData[0]);
+            this.Name = userData[1];
+            this.Email = userData[2];
+            this.Password = userData[3];
+            this.Goal = userData[4];
+            this.ActivityLevel = userData[5];
+            this.WeightKg = decimal.Parse(userData[6]);
+            this.HeightCm = decimal.Parse(userData[7]);
+            this.Age = int.Parse(userData[8]);
+            this.Sex = userData[9];
+            this.DietType = userData[10];
         }
 
         /// <summary>
-        /// Gets or sets the name of the user.
+        /// Gets or sets the user identifier.
         /// </summary>
         /// <value>
-        /// The name of the user.
+        /// The user identifier.
         /// </value>
-        public string UserName { get; set; }
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
+        public string Email { get; set; }
 
         /// <summary>
         /// Gets or sets the password.
@@ -40,7 +94,63 @@
         /// <value>
         /// The password.
         /// </value>
+
         public string Password { get; set; }
 
+        /// <summary>
+        /// Gets or sets the goal.
+        /// </summary>
+        /// <value>
+        /// The goal.
+        /// </value>
+        public string Goal { get; set; }
+
+        /// <summary>
+        /// Gets or sets the activity level.
+        /// </summary>
+        /// <value>
+        /// The activity level.
+        /// </value>
+        public string ActivityLevel { get; set; }
+
+        /// <summary>
+        /// Gets or sets the weight kg.
+        /// </summary>
+        /// <value>
+        /// The weight kg.
+        /// </value>
+        public decimal WeightKg { get; set; }
+
+        /// <summary>
+        /// Gets or sets the height cm.
+        /// </summary>
+        /// <value>
+        /// The height cm.
+        /// </value>
+        public decimal HeightCm { get; set; }
+
+        /// <summary>
+        /// Gets or sets the age.
+        /// </summary>
+        /// <value>
+        /// The age.
+        /// </value>
+        public int Age { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sex.
+        /// </summary>
+        /// <value>
+        /// The sex.
+        /// </value>
+        public string Sex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the diet.
+        /// </summary>
+        /// <value>
+        /// The type of the diet.
+        /// </value>
+        public string DietType { get; set; }
     }
 }
