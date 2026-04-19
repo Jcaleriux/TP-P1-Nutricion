3a9ca80d98081683075f119a6db673d6ea3db226
Jordy Sibaja Morales <jordy.sibaja@hasahospitality.com>
2026-04-18 18:11:13 -0600

    implement LoginView in Blazor

---
+++ src/ClassBlazor/Serivces/SessionService.cs
@@ -0,0 +1,9 @@
+﻿using ClassModels;
+
+namespace ClassBlazor.Services
+{
+    public class SessionService
+    {
+        public User? CurrentUser { get; set; }
+    }
+}
--- src/ClassViews/LoginView.Designer.cs
+++ src/ClassViews/LoginView.Designer.cs
@@ -38,9 +38,10 @@
             // 
             // btnLogin
             // 
-            btnLogin.Location = new Point(228, 227);
+            btnLogin.Location = new Point(261, 303);
+            btnLogin.Margin = new Padding(3, 4, 3, 4);
             btnLogin.Name = "btnLogin";
-            btnLogin.Size = new Size(316, 47);
+            btnLogin.Size = new Size(361, 63);
             btnLogin.TabIndex = 0;
             btnLogin.Text = "Login";
             btnLogin.UseVisualStyleBackColor = true;
@@ -48,9 +49,10 @@
             // 
             // btnRegister
             // 
-            btnRegister.Location = new Point(228, 294);
+            btnRegister.Location = new Point(261, 392);
+            btnRegister.Margin = new Padding(3, 4, 3, 4);
             btnRegister.Name = "btnRegister";
-            btnRegister.Size = new Size(316, 47);
+            btnRegister.Size = new Size(361, 63);
             btnRegister.TabIndex = 1;
             btnRegister.Text = "Register new user";
             btnRegister.UseVisualStyleBackColor = true;
@@ -59,46 +61,49 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(228, 109);
+            label1.Location = new Point(261, 145);
             label1.Name = "label1";
-            label1.Size = new Size(36, 15);
+            label1.Size = new Size(46, 20);
             label1.TabIndex = 2;
             label1.Text = "Email";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(231, 157);
+            label2.Location = new Point(264, 209);
             label2.Name = "label2";
-            label2.Size = new Size(57, 15);
+            label2.Size = new Size(70, 20);
             label2.TabIndex = 3;
             label2.Text = "Password";
             // 
             // txtEmail
             // 
-            txtEmail.Location = new Point(228, 127);
+            txtEmail.Location = new Point(261, 169);
+            txtEmail.Margin = new Padding(3, 4, 3, 4);
             txtEmail.Name = "txtEmail";
-            txtEmail.Size = new Size(316, 23);
+            txtEmail.Size = new Size(361, 27);
             txtEmail.TabIndex = 4;
             // 
             // txtPassword
             // 
-            txtPassword.Location = new Point(228, 175);
+            txtPassword.Location = new Point(261, 233);
+            txtPassword.Margin = new Padding(3, 4, 3, 4);
             txtPassword.Name = "txtPassword";
-            txtPassword.Size = new Size(316, 23);
+            txtPassword.Size = new Size(361, 27);
             txtPassword.TabIndex = 5;
             // 
             // LoginView
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
             Controls.Add(txtPassword);
             Controls.Add(txtEmail);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(btnRegister);
             Controls.Add(btnLogin);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "LoginView";
             Text = "LoginView";
             ResumeLayout(false);
